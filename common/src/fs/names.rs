08e731cfe1b0a1e2724eb91aade81326005b389a
Bauduin Raphaël <raphael@asfaload.com>
2025-09-11 15:03:23 +0200

    fix(common): use right suffix for signatures file

--- common/src/fs/names.rs
+++ common/src/fs/names.rs
@@ -1,3 +1,9 @@
-pub const SIGNATURES_SUFFIX: &str = "signatures";
+// FIXME: we could improve this by reusing defined const in furture consts,
+// but it seems too much of a burden at this time.
+// We might want to look at https://crates.io/crates/constcat
 pub const PENDING_SUFFIX: &str = "pending";
+
+pub const SIGNATURES_SUFFIX: &str = "signatures.json";
+pub const PENDING_SIGNATURES_SUFFIX: &str = "signatures.json.pending";
 pub const SIGNERS_DIR: &str = "asfaload.signers";
+pub const PENDING_SIGNERS_DIR: &str = "asfaload.signers.pending";
