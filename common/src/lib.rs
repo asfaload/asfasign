e6bd4e6f982e4a7d5fef72d822b1936e8dad737e
Bauduin Raphaël <raphael@asfaload.com>
2025-09-26 14:25:42 +0200

    refactor(common): reader based sha512 compute

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -1,6 +1,8 @@
 pub mod fs;
 
 use sha2::{Digest, Sha512, digest::typenum};
+use std::fs::File;
+use std::io::{BufReader, Read};
 use std::path::Path;
 
 pub enum AsfaloadHashes {
@@ -11,6 +13,28 @@ pub fn sha512_for_content(content: Vec<u8>) -> AsfaloadHashes {
 }
 
 pub fn sha512_for_file<P: AsRef<Path>>(path_in: P) -> Result<AsfaloadHashes, std::io::Error> {
-    let file_content = std::fs::read(path_in.as_ref())?;
-    Ok(sha512_for_content(file_content))
+    let file = File::open(path_in.as_ref())?;
+    let mut reader = BufReader::new(file);
+    let mut hasher = Sha512::new();
+    let mut buffer = [0u8; 8192];
+    let mut total_bytes_read = 0;
+
+    loop {
+        let bytes_read = reader.read(&mut buffer)?;
+        if bytes_read == 0 {
+            break; // End of file
+        }
+
+        total_bytes_read += bytes_read;
+        hasher.update(&buffer[..bytes_read]);
+    }
+
+    if total_bytes_read == 0 {
+        return Err(std::io::Error::new(
+            std::io::ErrorKind::InvalidData,
+            "We don't compute the sha of an empty value",
+        ));
+    }
+    let result = hasher.finalize();
+    Ok(AsfaloadHashes::Sha512(result))
 }
