dd403398c8971377890178577e6480e28275968d
Bauduin Raphaël <raphael@asfaload.com>
2026-01-08 16:40:14 +0100

    fix(rest-api): rename github project authenticator to validator
    There's no authentication going on...

--- rest-api/src/file_auth/github/mod.rs
+++ rest-api/src/file_auth/github/mod.rs
@@ -1,3 +1,5 @@
+pub mod actors;
+
 use std::path::PathBuf;
 use thiserror::Error;
 
--- rest-api/src/state.rs
+++ rest-api/src/state.rs
@@ -2,11 +2,15 @@ use std::path::PathBuf;
 
 use kameo::actor::{ActorRef, Spawn};
 
-use crate::actors::{
-    git_actor::GitActor, github_project_authenticator::GitHubProjectAuthenticator,
-    nonce_cache_actor::NONCE_CACHE_DB, nonce_cache_actor::NonceCacheActor,
-    nonce_cleanup_actor::NonceCleanupActor, repo_handler::RepoHandler,
-    signers_initialiser::SignersInitialiser,
+use crate::{
+    actors::{
+        git_actor::GitActor,
+        nonce_cache_actor::{NONCE_CACHE_DB, NonceCacheActor},
+        nonce_cleanup_actor::NonceCleanupActor,
+        repo_handler::RepoHandler,
+        signers_initialiser::SignersInitialiser,
+    },
+    file_auth::github::actors::github_project_validator::GitHubProjectValidator,
 };
 
 #[derive(Clone)]
@@ -15,7 +19,7 @@ pub struct AppState {
     pub git_actor: ActorRef<GitActor>,
     pub nonce_cache_actor: ActorRef<NonceCacheActor>,
     pub nonce_cleanup_actor: ActorRef<NonceCleanupActor>,
-    pub github_project_authenticator: ActorRef<GitHubProjectAuthenticator>,
+    pub github_project_validator: ActorRef<GitHubProjectValidator>,
     pub signers_initialiser: ActorRef<SignersInitialiser>,
     pub repo_handler: ActorRef<RepoHandler>,
 }
@@ -29,7 +33,7 @@ pub async fn init_state(git_repo_path: std::path::PathBuf) -> AppState {
     let nonce_cache_actor = NonceCacheActor::spawn(nonce_db_path.clone());
     let nonce_cleanup_actor = NonceCleanupActor::spawn(nonce_db_path);
 
-    let github_project_authenticator = GitHubProjectAuthenticator::spawn(());
+    let github_project_authenticator = GitHubProjectValidator::spawn(());
     let signers_initialiser = SignersInitialiser::spawn(());
 
     let repo_handler = RepoHandler::spawn((git_repo_path.clone(), git_actor.clone()));
@@ -39,7 +43,7 @@ pub async fn init_state(git_repo_path: std::path::PathBuf) -> AppState {
         git_actor,
         nonce_cache_actor,
         nonce_cleanup_actor,
-        github_project_authenticator,
+        github_project_validator: github_project_authenticator,
         signers_initialiser,
         repo_handler,
     }
