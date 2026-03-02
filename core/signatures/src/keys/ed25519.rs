28e7e0f618821e4af5d4ad0cae4a94307cacc361
Bauduin Raphaël <raphael@asfaload.com>
2026-03-02 13:42:47 +0100

    refactor(signatures): rmeove unnecessary map_err calls

--- core/signatures/src/keys/ed25519.rs
+++ core/signatures/src/keys/ed25519.rs
@@ -30,12 +30,9 @@ fn encrypt_signing_key(
     password: &str,
     scrypt_log_n: u8,
 ) -> Result<String, KeyError> {
-    let der = signing_key
-        .to_pkcs8_der()
-        .map_err(|e| KeyError::CreationFailed(format!("PKCS#8 DER encoding failed: {}", e)))?;
+    let der = signing_key.to_pkcs8_der()?;
 
-    let pki = pkcs8::PrivateKeyInfo::try_from(der.as_bytes())
-        .map_err(|e| KeyError::CreationFailed(format!("PKCS#8 parsing failed: {}", e)))?;
+    let pki = pkcs8::PrivateKeyInfo::try_from(der.as_bytes())?;
 
     let mut salt = [0u8; 16];
     let mut iv = [0u8; 16];
@@ -102,11 +99,10 @@ impl<'a> AsfaloadKeyPairTrait<'a> for AsfaloadKeyPair<Ed25519KeyPair> {
     }
 
     fn secret_key(&self, password: &str) -> Result<Self::SecretKey, KeyError> {
-        let signing_key =
-            SigningKey::from_pkcs8_encrypted_pem(&self.key_pair.encrypted_pem, password.as_bytes())
-                .map_err(|e| {
-                    KeyError::CreationFailed(format!("PKCS#8 decryption failed: {}", e))
-                })?;
+        let signing_key = SigningKey::from_pkcs8_encrypted_pem(
+            &self.key_pair.encrypted_pem,
+            password.as_bytes(),
+        )?;
 
         Ok(AsfaloadSecretKey { key: signing_key })
     }
@@ -136,10 +132,7 @@ impl AsfaloadSecretKeyTrait for AsfaloadSecretKey<Ed25519SecretKey> {
 
     fn from_file<P: AsRef<Path>>(path: P, password: &str) -> Result<Self, KeyError> {
         let pem_content = fs::read_to_string(path)?;
-        let signing_key = SigningKey::from_pkcs8_encrypted_pem(&pem_content, password.as_bytes())
-            .map_err(|e| {
-            KeyError::CreationFailed(format!("PKCS#8 decryption failed: {}", e))
-        })?;
+        let signing_key = SigningKey::from_pkcs8_encrypted_pem(&pem_content, password.as_bytes())?;
         Ok(AsfaloadSecretKey { key: signing_key })
     }
 }
