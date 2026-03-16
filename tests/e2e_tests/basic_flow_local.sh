a8760af6f2738f11e2ce40ee5858044777aba9c5
Bauduin Raphaël <raphael@asfaload.com>
2026-03-16 16:41:25 +0100

    test(e2e): add missing cargo --quiet flag

--- tests/e2e_tests/basic_flow_local.sh
+++ tests/e2e_tests/basic_flow_local.sh
@@ -271,7 +271,7 @@ section "Updating Signers File"
 
 run_step_json "Update signers file with key0" \
     '.success == true' \
-    cargo run -- update-signers --secret-key "$KEY_0" -u "$backend" -p $key_password $(signers_file 2)
+    cargo run --quiet -- update-signers --secret-key "$KEY_0" -u "$backend" -p $key_password $(signers_file 2)
 
 expect_fail "Attempts to re-sign pending signers with key0 (should fail)" \
     cargo run --quiet -- sign-pending --secret-key "$KEY_0" -u "$backend" --password $key_password $(pending_signers_file)
