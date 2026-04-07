40f5d493675133b579f1891b9e616a8ec6b434f3
Bauduin Raphaël <raphael@asfaload.com>
2026-04-07 09:45:05 +0000

    feat(features_lib): re-export build_history_entry_for and validate_signers_chain_on_disk

--- core/features_lib/src/lib.rs
+++ core/features_lib/src/lib.rs
@@ -32,6 +32,7 @@ pub use signatures::types::AsfaloadSignatures;
 pub use signers_file::activate_signers_file;
 pub use signers_file::sign_signers_and_metadata_file;
 pub use signers_file::validate_history;
+pub use signers_file::{build_history_entry_for, validate_signers_chain_on_disk};
 pub use signers_file_types::{
     Forge, ForgeOrigin, HistoryEntry, HistoryFile, SignersConfigMetadata, SignersConfigOrigin,
     VerifiedForgeContent,
