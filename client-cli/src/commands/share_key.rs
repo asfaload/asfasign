779454b940190c8ca70ce94dd85bb0add450cba5
Bauduin Raphaël <raphael@asfaload.com>
2026-06-26 17:49:51 +0200

    feat(client-cli): share-key tries pub key if error
    If it fails loading a pubkey from the path passed, it tried loading from
    a path with .pub extension added. This allows users to export key even
    if they erroneously passed the path to the secret key

--- client-cli/src/commands/share_key.rs
+++ client-cli/src/commands/share_key.rs
@@ -2,12 +2,20 @@ use crate::commands::keys_helpers::share_pub_key_message;
 use crate::output::ShareKeyOutput;
 use anstream::println;
 use anyhow::{Context, Result};
+use features_lib::errors::keys::KeyError;
 use features_lib::{AsfaloadPublicKeyTrait, AsfaloadPublicKeys};
 use std::path::Path;
 
 pub fn handle_share_key_command(public_key: &Path, raw: bool, json: bool) -> Result<()> {
-    let pk = AsfaloadPublicKeys::from_file(public_key)
-        .with_context(|| format!("Error loading public key from {}", public_key.display()))?;
+    let pk = match AsfaloadPublicKeys::from_file(public_key) {
+        Ok(k) => k,
+        Err(KeyError::ParseError(_)) => {
+            AsfaloadPublicKeys::from_file(public_key.with_extension("pub")).with_context(|| {
+                format!("Error loading public key from {}", public_key.display())
+            })?
+        }
+        Err(e) => return Err(e.into()),
+    };
     let pk_string = pk.to_base64();
     match (raw, json) {
         (_, true) => {
